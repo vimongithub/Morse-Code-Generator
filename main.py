b9a9a6608708af44d1c20825cd72a9938f30ea68
Vimal Pathak <pathakvimal1991@hotmail.com>
2022-08-24 19:37:15 +0530

    Morse Code Generator Version 1

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 from morse_codes import morse_codes
 
-code = ''
-is_app = True
 
-while is_app:
-    choice = input("Type '1' to Start Program, type 'Q' for Quit: ").upper()
-    if choice == "1":
-        user_input = input(" Enter text to Morse-code: ").upper()
+def encrypt(message):
+    code = ''
+    for letter in message:
 
-        for letter in user_input:
-            if code == '':
-               code = morse_codes[letter]
-            elif letter == " ":
-                code += " " + "/" + " "
-            elif letter not in morse_codes:     #if letter is not Numberic or Alphabatic, It will print '#'
-                code += " " + "#"
+        if letter != ' ':
+            code += morse_codes[letter] + " "
+        else:
+            code += "/" + " "
+    return f'Morse Code is : {code}'
+
+
+def decrypt(message):
+    global sp
+    decode = ''
+    citext = ''
+
+    for code in message:
+
+        if code != ' ' and code != '/':
+            sp = 0
+            decode += code
+
+        elif code == ' ':
+            sp += 1
+            if sp == 2:
+                citext += " "
             else:
-                code += " " + morse_codes[letter]
-        print(f'Morse Code : {code}')
+                citext += (list(morse_codes.keys()))[list(morse_codes.values()).index(decode)]
+                decode = ''
+        else:
+            pass
+    citext += (list(morse_codes.keys()))[list(morse_codes.values()).index(decode)]
+    return citext
+
+
+
+
+
+
+
+is_app = True
+while is_app:
+    choice = input("Type 'E' to Encrypt,Type D to Decrypt, type 'Q' for Quit: ").upper()
+    if choice == "E":
+        string_input = input(" Enter text to Morse-code: ").upper()
+        print(encrypt(string_input))
+
+    elif choice == "D":
+        morse_input = input("Enter Morse-code to decrypt: ")
+        print(decrypt(morse_input))
 
     elif choice == 'Q':
         is_app = False
         print("Thank you for using App, See you soon :)")
 
-    else:
-        choice = input("Type '1' to Start Program, type 'Q' for Quit: ").upper()
+    elif choice != ["D", "E", "Q"]:
+        print("Please enter from the available options.")
 
