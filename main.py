79166f84766a796d7dde9990e3a35aa4f80c014b
Vimal Pathak <pathakvimal1991@hotmail.com>
2022-08-30 17:11:28 +0530

    Morse Code Generator & Decoder Version 1.1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from morse_codes import morse_codes
+from morse_codes import morse_codes, alpha_nums
+from art import logo
+
+print(logo)
 
 def encrypt(message):
     code = ''
@@ -8,7 +11,7 @@ def encrypt(message):
             code += morse_codes[letter] + " "   #add morse code from dictionary plus a space
         else:
             code += "/" + " " #if word completes it will add / plus space as per morse-code rule
-    return f'Morse Code is : {code}'
+    return f'Morse Code of {message} is : {code}\n'
 
 
 def decrypt(message):
@@ -33,23 +36,37 @@ def decrypt(message):
             pass  #if it will find '/' it will skip that character
     citext += (list(morse_codes.keys()))[list(morse_codes.values()).index(decode)] #convert last character into morse-code
 
-    return citext
+    return f'Descryption of {message} is : {citext}\n '
 
 is_app = True
 while is_app:
-    choice = input("Type 'E' to Encrypt,Type D to Decrypt, type 'Q' for Quit: ").upper()
-    if choice == "E":
-        string_input = input(" Enter text to Morse-code: ").upper()
-        print(encrypt(string_input))
+    try:
+        choice = input("\n--->> Type 'E' to Encrypt,Type D to Decrypt, type 'Q' for Quit :: ")
+        if choice == "E" or choice == 'e':
+            string_input = input("\n ==> Enter text to Morse-code: ").upper()
+            for letter in string_input:
+                if letter in alpha_nums:
+                    if letter is string_input[-1]:
+                        print(encrypt(string_input))
+                else:
+                    raise ValueError
 
-    elif choice == "D":
-        morse_input = input("Enter Morse-code to decrypt: ")
-        print(decrypt(morse_input))
+        elif choice == "D" or choice == 'd':
+            morse_input = input("\n==> Enter Morse-code to decrypt: ").upper()
 
-    elif choice == 'Q':
-        is_app = False
-        print("Thank you for using App, See you soon :)")
+            if morse_input not in alpha_nums:
+                print(decrypt(morse_input))
+            else:
+                raise RuntimeError
 
-    elif choice != ["D", "E", "Q"]:
-        print("Please enter from the available options.")
+        elif choice == 'Q' or choice == 'q':
+            is_app = False
+            print("Thank you for using App, See you soon :)")
 
+        elif choice != ["D", "E", "Q", 'd', 'e', 'q']:
+            print("Please enter from the available options.")
+
+    except ValueError:
+        print("Please Enter String letters and Numbers only.")
+    except RuntimeError:
+        print("Please Enter valid Morse-code with proper spacing")
